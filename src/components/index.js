adde3427320ccfa5907bfd8052a3c82528ad80c9
Phonicia Anne Peramaiyan <153049020+PhoniciaAnne@users.noreply.github.com>
2024-07-12 00:11:58 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from "./Hero";
+import Hero from "./hero";
 import Navbar from "./Navbar";
 import About from "./About";
 import Tech from "./Tech";
