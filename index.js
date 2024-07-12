fa5e37f5e0aa99c81f28a419c7c83b6014ade96c
Phonicia Anne Peramaiyan <153049020+PhoniciaAnne@users.noreply.github.com>
2024-07-12 22:41:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,449 +1,128 @@
-import { SiFastapi } from "react-icons/si";
-import {
-  mobile,
-  backend,
+import logo from "./logo.svg";
+import logo1 from "./logo1.png";
+import backend from "./backend.png";
 
-  web,
-  javascript,
-  typescript,
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import iq from "./iq.jpeg";
+import flask from "./flask.png";
+// import solidity from "./Solidity.svg";
+import matplotlib from "./matplotlib.png";
+import blockchain from "./blockchain.png";
+import vercel from "./vercel.png";
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import Tensorflow from "./Tensorflow.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import pandas from "./pandas.png";
+import cooperation from "./cooperation.png";
+import reactjs from "./tech/reactjs.png";
+// import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import trackersy from "./trackersy.png";
+import hawkershut from "./HawkersHut.png";
 
-  ieee,
-  critiq,
-  html,
-  css,
-  reactjs,
+
+
+
+
+import gdsc from "./gdsc.jpeg";
+import open from "./open.jpeg";
+
+
+import admin from "./admin.png";
+import holopin from "./holopin.png";
+import h4b from "./h4b.jpeg";
+import nit from "./nit.jpeg";
+import quad from "./quad.jpeg"
+import oil from "./oil.png";
+import ieee from "./ieee.png";
+import critiq from "./critiq.png";
+
+import hh from "./hh.jpeg";
+import chat from "./chat.png";
+import python from "./python.png";
+import linux from "./linux.jpeg";
+import Sql from "./Sql.png";
+import pytorch from "./pytorch.png";
+import fastapi from "./fastapi.png";
+import idea from "./idea.png";
+ 
+  gdsc,
+  open,
   
-  tailwind,
-  pandas,
-  cooperation,
-  mongodb,
-  git,
-  Tensorflow,
-  docker, 
   
-  
-  
-  threejs,
+ 
+  admin
+export {
   idea,
   fastapi,
   pytorch,
-  Sql,
   linux,
   python,
   chat,
-
+  
+  
+  
+  logo,
+  logo1,
+  backend,
+ 
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  Tensorflow,
+  git,
+  html,
+  javascript,
+  mongodb,
+  pandas,
+  cooperation,
   iq,
   flask,
   // solidity,
   matplotlib,
   blockchain,
   vercel,
+  reactjs,
+  
+  tailwind,
+  ieee,
+  critiq,
+  typescript,
+  threejs,
+ 
+
+  Sql,
+ 
   trackersy,
   
   hawkershut,
+  
+  
   gdsc,
   open,
-  admin,
  
-  
+ 
+  admin,
   holopin,
   h4b,
-  nit,
   quad,
+  nit,
   oil,
+
   hh,
-  
-
-  
-  
-  
-  
-} from "../assets";
-import { FcCollaboration } from "react-icons/fc";
-
-
-
-export const navLinks = [
-  {
-    id: "about",
-    title: "About",
-  },
-  {
-    id: "work",
-    title: "Projects",
-  },
-  {
-    id: "experience",
-    title: "Experience",
-  },
-  {
-    id: "achievements",
-    title: "Achievements",
-  },
-  {
-    id: "contact",
-    title: "Contact",
-  },
-];
-
-const services = [
-  {
-    title: "Machine Learning",
-    icon: web,
-  },
-  {
-    title: "Artificial Intelligence",
-    icon: mobile,
-  },
-  {
-    title: "Full Stack Developer",
-    icon: backend,
-  },
-  {
-    title: "Deep Learning",
-    icon: blockchain,
-  },
-];
-
-const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
-    name: "JavaScript",
-    icon: javascript,
-  },
-  {
-    name: "React JS",
-    icon: reactjs,
-  },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
-  },
-  {
-    name: "Pandas",
-    icon: pandas,
-  },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
-    name: "git",
-    icon: git,
-  },
-  {
-    name: "TensorFlow",
-    icon: Tensorflow,
-  },
-  {
-    name: "Docker",
-    icon: docker,
-  },
-  {
-    name: "MatplotLib",
-    icon: matplotlib,
-  },
-  {
-    name: "Vercel",
-    icon: vercel,
-  },
-  {
-    name: "Flask",
-    icon: flask,
-  },
-  {
-    name: "FastAPI",
-    icon: fastapi,
-  },
-  {
-    name: "PyTorch",
-    icon: pytorch,
-  },
-  {
-    name: "Linux",
-    icon: linux,
-  },
-  {
-    name: "Sql",
-    icon: Sql,
-  },
-  {
-    name: "Problem-Solving",
-    icon: idea,
-  },
-  {
-    name : "Collaboration",
-    icon : cooperation ,
-
-  },
-  {
-    name: "Communication",
-    icon: chat,
-  },
-  {
-    name: "Python",
-    icon: python,
-  },
-];
-
-const experiences = [
-  {
-    title: "Software Engineering Intern",
-    company_name: "Kairos Technologies",
-    icon: iq,
-    iconBg: "#383E56",
-    date: "May 2024 - June 2024",
-    points: [
-      "Developed a production-level chatbot for Western Union's contact us page, serving regions like the US and India.",
-      "Successfully integrated advanced technologies to create a robust and efficient chatbot solution.",
-      "Collaborated with team members to refine chatbot responses and improve user interaction.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-      "Integrated the chatbot with existing Western Union systems using Flask.",
-    ],
-  },
-  {
-    title: "Research Intern",
-    company_name: "National Institute of Technology (NIT), Warangal ",
-    icon: nit,
-    iconBg: "#383E56",
-    date: "May 2024 - July 2024",
-    points: [
-      "• Enhance disease prediction accuracy using deep learning and advanced data preprocessing",
-      "Proposed a label encoding technique with 99% accuracy and a one-hot encoding technique with 100% accuracy.",
-      "Demonstrated proficiency in data manipulation, neural network construction, and model evaluation..",
-    ]
-  },
-  {
-    title: "Full Stack Web Developer - Mern",
-    company_name: "Prepinsta",
-    icon: gdsc,
-    iconBg: "#383E56",
-    date: "Jan 2024 - April 2024",
-    points: [      
-      " Developed and maintained front-end and back-end components of web applications.",
-      "DevelopedresponsivewebpagesusingHTML,CSS,andJavaScript; integratedbackendAPI",
-      "Successfully delivered multiple web pages that were both responsive and interactive."    
-    ],
-  },
-  {
-    title: "Marketing Lead",
-    company_name: "Gitam - Hyderabad",
-    icon: open,
-    iconBg: "#383E56",
-    date: "Jan 2024 - Ongoing",
-    points: [      
-      "Drove a 25% increase in eClub engagement through strategic marketing campaigns, tactics, and dynamic team coordination.",
-      "Working on Startegies with the community.",
-    ],
-  },
-];
-const achieve = [
-  {
-    title: "Amazon ML School'24 ",
-    company_name: "Amazon",
-    icon: h4b,
-    iconBg: "#383E56",
-    date: "July, 2024",
-    points: [
-      "Selected amoung 85000+ participants all over India",
-    ],
-  },
-   {
-    title: " Finalist in Pravaah AI Hackathon ",
-    company_name: "Quadrant Technologies",
-    icon: quad,
-    iconBg: "#383E56",
-    date: "May, 2024",
-    points: [      
-      "Been top 5 in AI hackathon leveraging technology with innovative Idea ",      
-    ],
-  },
-  {
-    title: "Participated in  Google Girl Hackathon",
-    company_name: "Google",
-    icon: holopin,
-    iconBg: "#383E56",
-    date: "April 2024",
-    points: [
-      "Had an oppurtunity to participate in biggest hackathon for Girls.",
-    ],
-  },
-  {
-    title: "B certificate in NCC",
-    company_name: "National Cadet Corps",
-    icon: hh,
-    iconBg: "#383E56",
-    date: "sep 2021",
-    points: [      
-      "been into NCC for 2 years and gained self descipline and focus ",
-    ],
-  },
-  
-  {
-    title: "Qualified for SUSTAIN-A-THON 2024",
-    company_name: "IndianOil",
-    icon: oil,
-    iconBg: "#383E56",
-    date: "April 2024",
-    points: [      
-      "Been into Sustainability Hackathon by Indian Oil Corporation Ltd",
-    ],
-  },
-];
-
-const testimonials = [
-  {
-  //   testimonial:
-  //     "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-  //   name: "Sara Lee",
-  //   designation: "CFO",
-  //   company: "Acme Co",
-  //   image: "https://randomuser.me/api/portraits/women/4.jpg",
-  // },
-  // {
-  //   testimonial:
-  //     "I've never met a web developer who truly cares about their clients' success like Rick does.",
-  //   name: "Chris Brown",
-  //   designation: "COO",
-  //   company: "DEF Corp",
-  //   image: "https://randomuser.me/api/portraits/men/5.jpg",
-  // },
-  // {
-  //   testimonial:
-  //     "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-  //   name: "Lisa Wang",
-  //   designation: "CTO",
-  //   company: "456 Enterprises",
-  //   image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-];
-
-const projects = [
-  {
-    name: "CHITTI",
-    description:
-      "AI-powered chatbot for mental health, utilizing cutting-edge NLP technologies and Tensor Flow/Keras models trained on an exclusive dataset.",
-                             
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "NLP",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Keras",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "ML",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Tensorflow",
-        color: "blue-text-gradient",
-      },
-    ],
-    image: critiq,
-    link: "https://www.canva.com/design/DAGF3gku9-c/7haBtnTJgovpzPw7_c1imw/view?utm_content=DAGF3gku9-c&utm_campaign=designshare&utm_medium=link&utm_source=editor",
-    git:"https://github.com/PhoniciaAnne/Mental-Health"
-  },  
-  {
-    name: "SurplusServe",
-    description:
-      " A platform for managing food waste, facilitating connections between food donors and recipients to address both food waste.",
-
-    tags: [
-      {
-        name: "reactjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "express",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Material-UI",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "TailwindCSS",
-        color: "green-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "Node.js",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: hawkershut,
-    link: "https://www.canva.com/design/DAGG9asTH58/pzogrnPMTISMhn1rCuk7XA/view?utm_content=DAGG9asTH58&utm_campaign=designshare&utm_medium=link&utm_source=editor",
-    git: "https://github.com/PhoniciaAnne/SurplusServe"
-  },
-  {
-    name: "Financial Analyst",
-    description:
-      " Built a tool utilizing the Gemini API for giving suggestions in financial analysis for AMEX Hackathon",
-    tags: [
-      {
-        name: "StreamLit",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "Gemini API",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-     
-    ],
-    image: ieee,
-    link: "https://demoai.streamlit.app",
-    github: "https://www.canva.com/design/DAGEMn9deHM/i4YBrvFeIl_P-ZoPNz65kg/edit?utm_content=DAGEMn9deHM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
-  },
-  {
-    name: "User-Chat-Application",
-    description:
-      "a simple and intuitive chat application designed to connect users effortlessly. This project leverages PHP and JavaScript to create a dynamic and responsive user experience.",
-    tags: [
-      {
-        name: "PHP",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "CSS",
-        color: "green-text-gradient",
-      },
-      {
-        name: "HTML",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "Javascript",
-        color: "green-text-gradient",
-      },
-    
-    ],
-    image: trackersy,
-
-    git:"https://github.com/PhoniciaAnne/User-Chat-Application"
-  },
-
-
- ];
-
-export { services, technologies, experiences, testimonials, projects, achieve };
+ 
+};
